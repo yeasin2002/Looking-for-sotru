56b3bc86c14a5ec0f9755d5824bd634553eb78eb
yeasin2002 <mdkawsarislam2002@gmail.com>
2024-09-17 23:35:49 +0600

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClientProvider } from "@/components/client";
-import { Noice } from "@/components/shared";
 import { hindSiliguriFont } from "@/fonts";
+import babor from "@/assets/images/babor/babor.jpeg";
 
 export const metadata: Metadata = {
   title: "লুৎফুজ্জামান বাবর",
   description:
     "লুৎফুজ্জামান বাবর (জন্ম: ১০ অক্টোবর ১৯৫৮) একজন বাংলাদেশি রাজনীতিবিদ। তিনি খালেদা জিয়ার মন্ত্রীসভায় নেত্রকোণা-৪ আসনের সংসদ সদস্য ও স্বরাষ্ট্র প্রতিমন্ত্রী ছিলেন ১০ অক্টোবর ২০০১ থেকে ২৯ অক্টোবর ২০০৬ পর্যন্ত।",
+  openGraph: {
+    title: "লুৎফুজ্জামান বাবর",
+    images: [{ url: babor.src, alt: "Lutfuzzaman Babar" }],
+  },
 };
 
 export default function RootLayout({
